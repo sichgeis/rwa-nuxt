c1e4a1da02da0f173ac0b82ccd1e02f50ec2902b
Christian Geisler <christian.geisler@hogrefe.com>
2023-02-26 21:13:41 +0100

    add nuxt composition api build module

--- nuxt.config.js
+++ nuxt.config.js
@@ -24,9 +24,7 @@ export default {
   css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-    '@/plugins/bootstrap-vue',
-  ],
+  plugins: ['@/plugins/bootstrap-vue'],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -35,6 +33,7 @@ export default {
   buildModules: [
     // https://go.nuxtjs.dev/typescript
     '@nuxt/typescript-build',
+    '@nuxtjs/composition-api/module',
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
